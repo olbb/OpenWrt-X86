8f5d652325340ec91cdfc61e95c00a0fa0515179
JuanLv <5554102+olbb@users.noreply.github.com>
2024-11-12 15:45:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -37,6 +37,12 @@ git clone https://github.com/olbb/openwrt-smartdns feeds/packages/net/smartdns/
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+#修改默认IP地址
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.10.1/g" ./package/base-files/files/bin/config_generate
+#修改默认主机名
+sed -i "s/hostname='.*'/hostname=Me-X86-Router/g" ./package/base-files/files/bin/config_generate
+#修改默认时区
+sed -i "s/timezone='.*'/timezone='CST-8'/g" ./package/base-files/files/bin/config_generate
+sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/OpenWrt/Me-X86-Router/g' package/base-files/files/bin/config_generate
