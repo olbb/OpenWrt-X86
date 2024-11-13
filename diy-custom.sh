b7890831c9a36490447142b7908a50d054f33b4a
JuanLv <5554102+olbb@users.noreply.github.com>
2024-11-13 14:44:38 +0800

    Create diy-custom.sh

---
+++ diy-custom.sh
@@ -0,0 +1,2 @@
+rm -rf feeds/luci/applications/luci-app-alist feeds/luci/applications/luci-app-ddns-go feeds/luci/applications/luci-app-passwall feeds/luci/applications/luci-app-smartdns
+rm -rf feeds/packages/net/alist feeds/packages/net/ddns-go feeds/packages/net/smartdns
