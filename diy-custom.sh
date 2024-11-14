f9411c49b2b37c2bb6bbb4e1ed435f087389908f
JuanLv <5554102+olbb@users.noreply.github.com>
2024-11-14 11:13:31 +0800

    Update diy-custom.sh

--- diy-custom.sh
+++ diy-custom.sh
@@ -1,4 +1,4 @@
-rm -rf feeds/luci/applications/luci-app-alist feeds/luci/applications/luci-app-ddns-go feeds/luci/applications/luci-app-passwall 
-# feeds/luci/applications/luci-app-smartdns
+rm -rf feeds/luci/applications/luci-app-alist feeds/luci/applications/luci-app-ddns-go feeds/luci/applications/luci-app-passwall feeds/luci/applications/luci-app-smartdns
 rm -rf feeds/packages/net/alist feeds/packages/net/ddns-go feeds/packages/net/smartdns
 git clone https://github.com/olbb/openwrt-smartdns.git feeds/packages/net/smartdns/
+git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns/
